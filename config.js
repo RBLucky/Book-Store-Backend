4538e0411fbb5d07009d1228a7ced078773c6ab8
RBLucky <nkosi.s.lucky@gmail.com>
2025-01-21 13:10:52 +0200

    remove string comment

--- config.js
+++ config.js
@@ -5,5 +5,3 @@ dotenv.config();
 export const PORT = process.env.PORT;
 
 export const mongoDBURL = process.env.MONGO_DB_URL;
-
-//mongodb+srv://root:root@books-store-mern.c7ba8z1.mongodb.net/books-collection?retryWrites=true&w=majority
